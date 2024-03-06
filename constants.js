2854f56dc8037df2f520fa82c10c40e47bbb67c4
mukhammad-yunus <dr.mukhammadyunus@gmail.com>
2024-03-06 15:37:44 +0100

    task completed
    Program is ready to use & deploy, but there are some organizations neede.

--- constants.js
+++ constants.js
@@ -2,7 +2,7 @@ export const data = [
   [
     `<div class="key" data-function="Delete"><i data-function="Delete" class="fa-solid fa-c"></i></div>`,
     `<div class="key" data-function="Backspace"><i  data-function="Backspace"class="fa-solid fa-delete-left"></i></div>`,
-    `<div class="key"  data-function="( )">( )</div>`,
+    `<div class="key" data-function="Parenthesis">( )</div>`,
     "/",
   ],
   ["7", "8", "9", "*"],
--- index.js
+++ index.js
@@ -2,10 +2,11 @@ import { displayKeyboard } from "./component.js";
 displayKeyboard();
 const inputEl = document.querySelector(".input");
 const resultEl = document.querySelector(".result");
-const initInputEl = `<div class="input"></div>`;
-let inputVal = "";
+let inputVal = "0";
 let result = "";
 let lastChar = "0";
+let lastNum = "";
+let parenthesis = 0;
 const keyEls = document.querySelectorAll(".key");
 keyEls.forEach((key) =>
   key.addEventListener("click", (e) => {
@@ -22,97 +23,244 @@ keyEls.forEach((key) =>
 window.addEventListener("keydown", (e) => {
   if (
     !(
-      /^[0-9+\-*/%()=]$/.test(e.key) ||
+      /^[0-9+\-*/()=.]$/.test(e.key) ||
       e.key === "Enter" ||
       e.key === "Backspace" ||
       e.key === "Delete"
     )
   )
     return;
-  if (
-    e.key === "(" ||
-    e.key === ")" ||
-    e.key === "Backspace" ||
-    e.key === "Delete"
-  ) {
+  if (e.key === "Backspace" || e.key === "Delete") {
     handleSpecialOp(e.key);
   } else {
-    
     handleDisplay(e.key);
   }
 });
 
 function calculate() {
-    lastChar = inputVal[inputVal.length-1];
-    const isOperator = ("+\-*/%").includes(lastChar);
-    console.log(isOperator);
-    if (isOperator) {
-        
-        inputVal = inputVal.slice(0, -1);
-        result = String(eval(`(${inputVal})`));
-        resultEl.textContent = result;
-        inputEl.textContent = inputVal;
-        //.round .toFixed, generally you have to make it responsive so that numbers should fit the space
-    } else{
-        result = String(eval(`(${inputVal})`));
-        resultEl.textContent = result;
-        //.round .toFixed, generally you have to make it responsive so that numbers should fit the space
-
-    }
-}
-
-function handleDisplay(key) {
-    const isZero = inputEl.innerText == "0";
-    const isOperator = ("+\-*/%").includes(key);
-    const isDoubleOp = ("+\-*/%").includes(lastChar)&&isOperator;
-    
-    if( (isZero && isOperator) ||isDoubleOp ) return;
-//Above prevents operators being used in the beginning or being used multiple times
-
-  if (key == "=" || key === "Enter") {
-    calculate();
-  } else {
-    if (result) {
-      inputEl.textContent = key;
-      inputVal = key;
-      lastChar = key;
-      result = "";
-      resultEl.innerText = "";
+  function calculationDisplay() {
+    let temp = String(result);
+    let isPoint = temp.includes(".") && temp.length > 14;
+    if (isPoint || temp.length > 14) {
+      result = String(result.toPrecision(14));
     } else {
-      if (inputEl.innerText == "0") {
-        inputEl.textContent = key;
-        inputVal = key;
-        lastChar = key;
-      } else {
-        inputEl.textContent += key;
-        inputVal += key;
-        lastChar = key;
+      result = temp;
+    }
+
+    resultEl.textContent = result;
+  }
+  if (parenthesis != 0) {
+    for (let i = 0; i < parenthesis; i++) {
+      inputVal += ")";
+      inputEl.textContent = inputVal;
+    }
+  }
+  lastChar = inputVal[inputVal.length - 1];
+  const isOperator = "+-*/".includes(lastChar);
+  if (isOperator) {
+    try {
+      inputVal = inputVal.slice(0, -1);
+      inputEl.textContent = inputVal;
+      result = eval(`(${inputVal})`);
+      if (isNaN(result)) {
+        throw new Error("error");
       }
+      calculationDisplay();
+    } catch (error) {
+      result = "error";
+      resultEl.textContent = result;
+    }
+  } else {
+    try {
+      result = eval(`${inputVal}`);
+      if (isNaN(result)) {
+        throw new Error("error");
+      }
+      calculationDisplay();
+    } catch (error) {
+      result = "error";
+      resultEl.textContent = result;
     }
   }
 }
 
+function handleDisplay(key) {
+  const isZero = inputEl.innerText == "0";
+  const isOperator = "+-*/".includes(key);
+  const isDoubleOp = "+-*/".includes(lastChar) && isOperator;
+  if (isDoubleOp) return;
+  //Above prevents operators being used multiple times
+  if (key == "=" || key === "Enter") {
+    if (!result) {
+      calculate();
+    }
+  } else {
+    if (result) {
+      if (isOperator && result != "error") {
+        inputEl.textContent = result + key;
+        inputVal = result + key;
+        lastChar = key;
+        lastNum = "";
+        result = "";
+        resultEl.innerText = result;
+        return;
+      } else if (isOperator) {
+        inputVal = "0";
+        inputEl.textContent = inputVal;
+        lastChar = "";
+        result = "";
+        resultEl.innerText = result;
+        return;
+      }
+
+      inputVal = key;
+      inputEl.textContent = inputVal;
+      lastChar = key;
+      result = "";
+      resultEl.innerText = "";
+    } else if (key == "(" || key == ")") {
+      handleParenthesis();
+    } else {
+      if (!isOperator) {
+        lastNum += key;
+      } else {
+        lastNum = "";
+      }
+      if ((isZero && !isOperator && key != ".") || (isZero && key == "0")) {
+        inputVal = key;
+        inputEl.textContent = inputVal;
+        lastChar = key;
+      } else {
+        inputVal += key;
+        inputEl.textContent = inputVal;
+        lastChar = key;
+      }
+      // if()
+    }
+  }
+}
 
 function handleSpecialOp(value) {
   switch (value) {
     case "Delete":
-      inputEl.innerText = "0";
+      inputVal = "0";
+      inputEl.innerText = inputVal;
+      lastNum = "";
       result = "";
       resultEl.innerText = "";
       break;
     case "Backspace":
-      if (result) {
-        inputEl.innerText = "0";
-        result = "";
-        resultEl.innerText = "";
-      } else {
-        if (inputEl.innerText == "0" || inputEl.innerText.length == 1) {
-            inputEl.innerText = "0"
-        } else{
-            inputEl.innerText = inputEl.innerText.slice(0, -1);
-            resultEl.innerText = "";
-            result = "";
-        }
-      }
+      handleBackspace();
+      break;
+    case "invert":
+      if (!lastNum) return;
+      toggleNumber(lastNum);
+      break;
+    case "Parenthesis":
+      handleParenthesis();
+      break;
   }
+}
+
+function handleBackspace() {
+  if (result && result != "error") {
+    inputVal = "0";
+    inputEl.textContent = inputVal;
+    result = "";
+    lastNum = "";
+    resultEl.textContent = "";
+  } else {
+    if (result == "error") {
+      result = "";
+      resultEl.textContent = result;
+    }
+    if (inputVal == "0" || inputVal.length == 1) {
+      inputVal = "0";
+      inputEl.textContent = inputVal;
+      lastNum = "";
+    } else {
+      inputVal = inputEl.textContent.slice(0, -1);
+      inputEl.textContent = inputVal;
+      const lastIndex = inputVal.length - 1;
+      lastChar = inputVal[lastIndex];
+      lastNum = findLastNumber(inputVal);
+      console.log(lastNum);
+    }
+  }
+}
+
+function findLastNumber(expression) {
+  const reversed = expression.split("").reverse();
+
+  let lastNumber = "";
+  let foundOperator = false;
+  for (const char of reversed) {
+    if ("0123456789.".includes(char)) {
+      lastNumber = char + lastNumber;
+    } else if (`\+-*/`) {
+      foundOperator = true;
+      break;
+    }
+  }
+  return foundOperator ? lastNumber : "";
+}
+function toggleNumber(num) {
+  const invertNum = -1 * parseFloat(num);
+
+  if (invertNum > 0) {
+    let index = `(${lastNum})`.length * -1;
+    inputVal = inputVal.slice(0, index) + invertNum;
+    inputEl.textContent = inputVal;
+    lastNum = String(invertNum);
+  } else {
+    let index = lastNum.length * -1;
+    inputVal = `${inputVal.slice(0, index)}(${invertNum})`;
+    inputEl.textContent = inputVal;
+    lastNum = String(invertNum);
+  }
+}
+
+function handleParenthesis() {
+  const isOperator = "+-*/".includes(lastChar);
+
+  if (lastNum) {
+    if (!isOperator && parenthesis === 0) {
+      inputVal += "*(";
+      parenthesis++;
+      parenthesisDisplay(inputVal, "(");
+      return;
+    }
+    if (parenthesis == 0) return;
+    inputVal += ")";
+    parenthesis--;
+    parenthesisDisplay(inputVal, ")");
+  } else {
+    if (lastChar == ")" && !parenthesis) {
+      inputVal += "*(";
+      parenthesis++;
+      parenthesisDisplay(inputVal, "(");
+      return;
+    }
+    if (inputVal === "0" && lastChar != ")") {
+      inputVal = "(";
+      parenthesis++;
+      parenthesisDisplay(inputVal, "(");
+      return;
+    } else if (inputVal != "0" && lastChar != ")") {
+      inputVal += "(";
+      parenthesis++;
+      parenthesisDisplay(inputVal, "(");
+
+      return;
+    } else if (parenthesis > 0) {
+      inputVal += ")";
+      parenthesis--;
+      parenthesisDisplay(inputVal, ")");
+    }
+  }
+}
+function parenthesisDisplay(value, key) {
+  inputEl.textContent = value;
+  lastChar = key;
+  lastNum = "";
 }
